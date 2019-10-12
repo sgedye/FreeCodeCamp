67dfdc4b4941f1f99435361077171bbd7e201db7
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-12 17:52:38 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,7 +8,6 @@ function checkObj(checkProp) {
 }
 checkObj("gift");
 
-
 /* Accessing Nested Arrays */
 var myPlants = [
   { 
@@ -76,4 +75,63 @@ function updateRecords(id, prop, value) {
 }
 updateRecords(2548, "artist", "");  // Alter these values to test the code
 
+/* While Loop */
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
 
+/* For Loop */
+var myArray = [];
+for (var i=1; i<6; i++) {
+    myArray.push(i);
+}
+console.log(myArray); //1,2,3,4,5
+
+/* Nested For Loops (multi-dimentional arrays) */
+function multiplyAll(arr) {
+  var product = 1;
+  for (var i=0; i<arr.length; i++) {
+    for (var j=0; j<arr[i].length; j++) {
+      product *= arr[i][j];
+      console.log(product)
+    }
+  }
+  return product;
+}
+multiplyAll([[1,2],[3,4],[5,6,7]]); //5040
+
+/* Profile Lookup */
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    }
+];
+function lookUpProfile(name, prop) {
+  for (var i=0; i<contacts.length; i++) {
+    if (name == contacts[i].firstName) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact"; 
+}
+lookUpProfile("Harry", "likes"); // Change these values to test your function
