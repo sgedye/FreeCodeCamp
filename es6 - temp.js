3b3b8b46e734d905a4efeeaccefbe5c4e56d2ef2
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-18 18:38:11 +0800

    Update es6 - temp.js

--- es6 - temp.js
+++ es6 - temp.js
@@ -9,7 +9,7 @@ const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2 = [...arr1];
 console.log(arr2);
 
-// #3
+// #3 Destructuring
 const HIGH_TEMPERATURES = {
   yesterday: 75,
   today: 77,
@@ -32,3 +32,19 @@ console.log(highToday); // should be 77
 console.log(highTomorrow); // should be 80
 
 // #5
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+const { today: {low: lowToday, high: highToday}} = LOCAL_FORECAST;
+console.log(lowToday); // should be 64
+console.log(highToday); // should be 77
+
+// #6
+let a = 8, b = 6;
+[a,b] = [b,a];
+console.log(a); // should be 6
+console.log(b); // should be 8
+
+// #7
