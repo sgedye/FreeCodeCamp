fd32ac62a797557a954e912aabaa76b9d409aa43
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-18 18:22:35 +0800

    Update es6 - temp.js

--- es6 - temp.js
+++ es6 - temp.js
@@ -8,3 +8,27 @@ console.log(sum(1, 2, 3)); // 6
 const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
 let arr2 = [...arr1];
 console.log(arr2);
+
+// #3
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+const { today, tomorrow } = HIGH_TEMPERATURES;
+console.log(yesterday); // should be not defined
+console.log(today); // should be 77
+console.log(tomorrow); // should be 80
+
+// #4
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+console.log(yesterday) // should be not defined
+console.log(highToday); // should be 77
+console.log(highTomorrow); // should be 80
+
+// #5
