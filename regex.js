019556129511d8f688a340e30cc7c65336f7c719
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-22 14:15:24 +0800

    Update regex.js

--- regex.js
+++ regex.js
@@ -26,3 +26,14 @@ let result = userCheck.test(username);
   $ - end of input
   i - ignore case of input
 */
+
+// Use lookaheads to match passwords that are greater than 5 characters long, do not begin with numbers, and have two consecutive digits.
+let sampleWord = "abc123";
+let pwRegex = /(?=\w{6,})(?=\D+\d{2})/i;
+let result = pwRegex.test(sampleWord);
+
+// A regex to check for the names of Franklin Roosevelt or Eleanor Roosevelt. Should be case sensitive and make concessions for middle names.
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/;
+let result = myRegex.test(myString);
+// the .* is used for the middle names (. = any char; * = zero or more times).
