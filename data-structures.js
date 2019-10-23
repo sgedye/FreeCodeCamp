80cda2437d727dc763ca1d9478ad1059e79b0a89
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-23 17:47:12 +0800

    Update data-structures.js

--- data-structures.js
+++ data-structures.js
@@ -1,12 +1,12 @@
 // Splice the array, so that sumOfTen([2,5,1,5,2,1]) = 10
 function sumOfTen(arr) {
-	console.log(`Arr: ${arr}`); //Output: [2,5,1,5,2,1]
-  arr.splice(4,2);
-	console.log(`Arr: ${arr}`); //Output: [2,5,1,5]
-  arr.splice(2,1);
-	console.log(`Arr: ${arr}`); //Output: [2,5,5]
-  arr.splice(0,1);
-	console.log(`Arr: ${arr}`); //Output: [5,5]
+  console.log(`Arr: ${arr}`); //Output: [2,5,1,5,2,1]
+    arr.splice(4,2);
+  console.log(`Arr: ${arr}`); //Output: [2,5,1,5]
+    arr.splice(2,1);
+  console.log(`Arr: ${arr}`); //Output: [2,5,5]
+    arr.splice(0,1);
+  console.log(`Arr: ${arr}`); //Output: [5,5]
   return arr.reduce((a, b) => a + b);
 }
 console.log(sumOfTen([2, 5, 1, 5, 2, 1])); //Output: 10
@@ -40,5 +40,4 @@ function countOnline(usersObj) {
 }
 let uObj = { Alan: { online: true }, Jeff: { online: false }, Sarah: { online: true } };
 countOnline(uObj);
-
-
+//END
