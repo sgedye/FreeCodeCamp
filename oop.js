eecb942b93987ec6eb26c6d679b18d97c10865e4
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-26 13:56:29 +0800

    Update oop.js

--- oop.js
+++ oop.js
@@ -1,18 +1,19 @@
 //Object Orientated Programming
 
-function Dog(name, color) {
+function Dog (name, color) {
   this.name = name,
-  this.color = color,
-  this.numLegs = 4
+  this.color = color
 };
 Dog.prototype = {
   constructor: Dog,
   numLegs: 4,
-  eating: function() {console.log(“Nom nom nom”);}
+  eating: function() { console.log("Nom nom nom"); },
+	describe: function() { console.log("My name is " + this.name); }
 };
-let hound = new Dog(“Boris”, “Black”);
-hound instanceof Dog; //Returns true
-hound.name; //Return
-hound.eating(); //Output: Nom nom nom
-Dog.prototype.isPrototypeOf(hound); //Returns true
-Object.prototype.isPrototypeOf(Dog); //Returns true
+let terrier = new Dog("Toby", "red");
+console.log(`Name: ${terrier.name} | Color: ${terrier.color} | Legs: ${terrier.numLegs}`);
+terrier.describe();
+terrier.eating();
+console.log(Dog.prototype.isPrototypeOf(terrier)); //Returns true
+console.log(terrier instanceof Dog); //Returns true
+console.log(Object.prototype.isPrototypeOf(Dog)); //Returns true
