51e7564702d2bc1c01707f1be0f7026ec2aaafa2
Shaun Gedye <sgedye88@yahoo.com.au>
2019-10-26 14:06:06 +0800

    Update oop.js

--- oop.js
+++ oop.js
@@ -8,7 +8,7 @@ Dog.prototype = {
   constructor: Dog,
   numLegs: 4,
   eating: function() { console.log("Nom nom nom"); },
-	describe: function() { console.log("My name is " + this.name); }
+  describe: function() { console.log("My name is " + this.name); }
 };
 let terrier = new Dog("Toby", "red");
 console.log(`Name: ${terrier.name} | Color: ${terrier.color} | Legs: ${terrier.numLegs}`);
@@ -17,3 +17,14 @@ terrier.eating();
 console.log(Dog.prototype.isPrototypeOf(terrier)); //Returns true
 console.log(terrier instanceof Dog); //Returns true
 console.log(Object.prototype.isPrototypeOf(Dog)); //Returns true
+
+//Using inheritance
+function Animal() { }
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+let beagle = Object.create(Animal.prototype);
+beagle.eat(); //Output: nom nom nom
