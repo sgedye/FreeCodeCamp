a3d8b0677123420947e7b54666d09d20b205253d
Shaun Gedye <sgedye88@yahoo.com.au>
2019-11-08 19:44:17 +0800

    Update algorithms2.js

--- algorithms2.js
+++ algorithms2.js
@@ -79,3 +79,28 @@ function translatePigLatin(str) {
     : str.concat("way");
 }
 translatePigLatin("consonant");
+
+// Matching a object (source) with the obects in an array (collection)
+function whatIsInAName(collection, source) {
+  var arr = [];	
+	let coLen = Object.keys(collection).length;
+	let searchLen = Object.entries(source).length;
+	for (let i=0; i<coLen; i++) {
+		let tempObj = collection[i];
+		let allMatch = true;
+		for (let j=0; j<searchLen; j++) {
+			let x = Object.entries(source)[j][0];
+			if (source[x] != tempObj[x]) {
+				allMatch = false;				
+			}		
+		}
+		if (allMatch) { 
+			arr.push(tempObj); 
+		}
+	}
+	return arr;
+}
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{"a": 1, "b": 2, "c": 3}], {"a": 1, "b": 9999, "c": 3});
+
