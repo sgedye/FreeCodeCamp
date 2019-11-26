df7624c3aba866a8a77cbd4922f19736db5c8da3
Shaun Gedye <sgedye88@yahoo.com.au>
2019-11-26 13:33:23 +1100

    Update f-end_React.jsx

--- f-end_React.jsx
+++ f-end_React.jsx
@@ -5,4 +5,22 @@ const MyComponent = () => {
   );
 };
 
-
+//Create a Component with Composition (a child component)
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+class ParentComponent extends React.Component {
+  constructor(props) { super(props); }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        <ChildComponent />
+      </div>
+    );
+  }
+};
