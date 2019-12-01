0e0ffe6362eaf657b3568e32c34e649ecc953d09
Shaun Gedye <sgedye88@yahoo.com.au>
2019-12-01 21:07:32 +1100

    Update f-end_React.jsx

--- f-end_React.jsx
+++ f-end_React.jsx
@@ -79,3 +79,16 @@ const ShoppingCart = (props) => {
 ShoppingCart.defaultProps = { items: 0 };
 
 
+class CampSite extends React.Component {
+  constructor(props) { super(props); }
+  render() {
+    return (
+      <div><Camper/></div>
+    );
+  }
+};
+const Camper = (props) => <p>{props.name}</p>
+Camper.defaultProps = { name: "CamperBot" }
+Camper.propTypes = { name: PropTypes.string.isRequired }
+
+
