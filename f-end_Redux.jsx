b2be25c7d0025c8879b267f8add287def1d02672
Shaun Gedye <sgedye88@yahoo.com.au>
2019-12-30 18:26:56 +1100

    Update f-end_Redux.jsx

--- f-end_Redux.jsx
+++ f-end_Redux.jsx
@@ -33,3 +33,28 @@ const rootReducer = Redux.combineReducers({
 });
 
 const store = Redux.createStore(rootReducer);
+
+
+//REVISION
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+// define constants for increment/decrement action types
+
+const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return (state += 1);
+        case DECREMENT:
+            return (state -= 1);
+        default:
+            return state;
+    };
+};
+// define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = () => { return {type: INCREMENT}; }; 
+const decAction = () => { return {type: DECREMENT}; }; 
+// define action creators for incrementing/decrementing
+
+const store = Redux.createStore(counterReducer); 
+// define the Redux store here, passing in your reducers
